3aa30f3e9e8c236452cee28dc219d9b1d6223e06
Luke Marshall <lukemarshall2222@gmail.com>
2023-10-14 13:32:48 -0700

    Commit draft server and pages

--- web/app.py
+++ web/app.py
@@ -1,14 +1,57 @@
 """
-John Doe's Flask API.
+Luke Marshall's Flask API.
 """
 
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+
+import os
+import configparser
+
+# config parser to find the port and debug mode from the config file
+def parse_config(config_paths):
+    """From project 0"""
+    config_path = None
+    for file in config_paths:
+        if os.path.isfile(file):
+            config_path = file
+            break
+
+    if config_path is None:
+        raise RuntimeError("Configuration file not found!")
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    return config
+
+# collect port and debug mode from the config file
+config = parse_config(["credentials.ini", "default.ini"])
+PORT = config["SERVER"]["PORT"]
+DEBUG = config["SERVER"]["DEBUG"]
+
+
+@app.route("/<string:filename>")
+def serve(filename):
+    if '~' in filename or '..' in filename: # check for illegal characters
+        Flask.abort(403)
+    path = f"pages/{filename}"
+    if path != "pages/" and os.path.exists(path):
+        # check the path exits and that is not just the directory 'pages' 
+        return Flask.send_from_directory()
+    elif path == "pages/" or not os.path.exists(path):
+        # check for nonexistent path or empty filename
+        Flask.abort(404)
+    return "This should not be shown, something went wrong during file checking\n"
+
+@app.errorhandler(403)
+def handle_illegal_characters(e):
+    return Flask.send_from_directory('pages/', '403.html'), 403
+
+@app.errorhandler(404)
+def handle_nonexistent_page(e):
+    return Flask.send_from_directory('pages/', '404.html'), 404
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=DEBUG, host='0.0.0.0', port=PORT)
